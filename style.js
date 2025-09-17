5fa9a30e334e5b35971907ba2a27b5cb65ef2aa7
Mohsin Hera <miamohsin.9911@gmail.com>
2025-09-17 12:51:54 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,41 @@
+const heartCountEl = document.getElementById('numbers');
+const love = document.getElementsByClassName('love');
+let count = 0;
+
+for (const heart of love) {
+  heart.addEventListener("click", () => {
+    count++; 
+    heartCountEl.textContent = count;
+
+    heart.classList.remove("fa-regular");
+    heart.classList.add("fa-solid", "text-red-500");
+  });
+}
+
+
+
+coins=100
+const coinEl = document.getElementById("coins");
+
+const callButtons = document.querySelectorAll(".btns");
+
+callButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const card = button.closest(".logo"); 
+    const serviceName = card.querySelector("h1").innerText;
+    const serviceNumber = card.querySelectorAll("h1")[1].innerText;
+    if (coins < 20) {
+      alert("Not enough coins! You need at least 20 coins.");
+      return;
+    }
+
+    alert(`Calling ${serviceName} - ${serviceNumber}`);
+
+    coins -= 20;
+    coinEl.textContent = coins;
+  });
+});
+
+
+
+
